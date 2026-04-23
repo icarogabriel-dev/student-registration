065e15c3dcf9554b5d2a954f36724a9b50e88805
Icaro Gabriel <icarog584@gmail.com>
2026-04-22 23:12:52 -0300

    feat: criação da classe aluno
    - adiciona os atributos matricula, nome e idade
    - cria construtor padrão e parametrizado
    - implementa getters e setters

---
+++ src/model/Aluno.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Aluno {
+	
+	private Integer matricula;
+	private String nome;
+	private Integer idade;
+	
+	public Aluno() {
+	}
+
+	public Aluno(Integer matricula, String nome, Integer idade) {
+		this.matricula = matricula;
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public Integer getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+}
